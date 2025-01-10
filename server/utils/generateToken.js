f84dcd06d61f0ba2dc47295d68d7a2a403add81c
Abuthahirvgd <abuthahirvgd@gmail.com>
2025-01-10 23:52:51 +0530

    Delete server directory

--- server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateToken = (id) => {
-    return jwt.single({id},process.env.JWT_SECRET,{
-        expiresIn: '30d'
-    })
-}
-
-export default generateToken
